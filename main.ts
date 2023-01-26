7142280aed6ced0cf77b8130df406b089f5e8d7e
nreyes29 <120482208+nreyes29@users.noreply.github.com>
2023-01-26 08:16:13 -0600

    boolean for beverages

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
 // Don't forget to comment your code as you work!
+let mySprite: Sprite = null
+let customer = ""
+let server = game.askForString("What drink would you like to have?")
+if (customer == "Water") {
+    mySprite = sprites.create(assets.image`water`, SpriteKind.Player)
+} else {
+    mySprite = sprites.create(assets.image`other drinks`, SpriteKind.Player)
+}
